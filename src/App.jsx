c529914f0909c3bb061656b6c67219aa16cf7bed
JUNHYUK JUNG <hasinofi@outlook.com>
2023-11-17 14:50:17 +0900

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import Banner from "./components/Banner";
 import Header from "./components/Header";
 import TopBanner from "./components/TopBanner";
 
 const App = () => {
-  // 색을 tailwind.config.js에서 지정한 색으로 변경
   return (
+    // 색을 tailwindcss에서 제공하는 색으로 변경
     <div className="font-pretendard text-techit-gray-300">
       <TopBanner />
       <Header />
-      <div className="bg-blue-100 h-[400px]">Banner</div>
+      <Banner />
       <section className="mt-20 bg-green-100 max-w-screen-xl mx-auto px-6">
         <div className="bg-red-100 h-[64.5px]">KDT</div>
         <div className="bg-purple-100 grid grid-cols-4 gap-6 justify-items-center">
